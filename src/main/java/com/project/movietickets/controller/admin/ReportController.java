b22e7ff27d34e5554988c6f85bc6d4d542597ad4
Vo Van Tam <vovantam.dev@gmail.com>
2020-09-15 20:57:21 +0700

    Update view report

--- src/main/java/com/project/movietickets/controller/admin/ReportController.java
+++ src/main/java/com/project/movietickets/controller/admin/ReportController.java
@@ -1,16 +1,17 @@
 package com.project.movietickets.controller.admin;
 
 import com.project.movietickets.model.Report;
+import com.project.movietickets.service.MovieService;
 import com.project.movietickets.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin")
@@ -19,18 +20,22 @@ public class ReportController {
 
     private final ReportService reportService;
 
-    @RequestMapping(value = "/reports", method = RequestMethod.GET)
+    private final MovieService movieService;
+
+    @GetMapping(value = "/reports")
     public ModelAndView index(
             @RequestParam(value = "startDate", required = false) String startDate,
-            @RequestParam(value = "startDate", required = false) String endDate,
+            @RequestParam(value = "endDate", required = false) String endDate,
             @RequestParam(value = "category", required = false) String category,
             ModelAndView modelAndView
     ) {
+        modelAndView.setViewName("admin/report/index");
 
         List<Report> reports = reportService.searchReport(startDate, endDate, category);
-
-        modelAndView.setViewName("admin/report/index");
         modelAndView.addObject("reports", reports);
+
+        Set<String> categories = movieService.getAllCategories();
+        modelAndView.addObject("categories", categories);
 
         return modelAndView;
     }
--- src/main/java/com/project/movietickets/repository/custom/SearchReportRepositoryImpl.java
+++ src/main/java/com/project/movietickets/repository/custom/SearchReportRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.project.movietickets.repository.custom;
 
 import com.project.movietickets.entity.TicketEntity;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -9,6 +10,7 @@ import javax.persistence.Query;
 import java.util.List;
 
 @Repository
+@Log4j2
 public class SearchReportRepositoryImpl implements SearchReportRepository {
 
     @PersistenceContext
@@ -22,19 +24,21 @@ public class SearchReportRepositoryImpl implements SearchReportRepository {
                 "on tickets.room_movie_schedule_id = room_movie_schedules.id\n" +
                 "inner join movies\n" +
                 "on room_movie_schedules.movie_id = movies.id\n" +
-                "where 1=1");
+                "where 1=1\n");
 
-        if (category != null && !category.isBlank()){
-            sqlBuilder.append("and movies.category = '" + category + "'");
+        if (category != null && !category.equals("ALL")){
+            sqlBuilder.append("and movies.category = '" + category + "'\n");
         }
 
-        if (startDate != null && endDate != null){
+        if (startDate != null && endDate != null && !startDate.isBlank() && !endDate.isBlank()){
             sqlBuilder.append("and tickets.date between '" +
                     startDate +
                     "' and '" +
                     endDate +
                     "'");
         }
+
+        log.info(sqlBuilder.toString());
 
         Query query = entityManager.createNativeQuery(sqlBuilder.toString(), TicketEntity.class);
 
